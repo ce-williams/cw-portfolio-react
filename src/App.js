b0ef9dbfc35972b7b72ccae7a72263f1cfe7399f
cewilliams <cewilliams@csbsju.edu>
2019-05-12 22:43:10 -0500

    react router, components added, react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>CW portfolio</h1>
-        <p>CD checkpoint - GH connected</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import Landing from './components/Landing';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <h1>CW portfolio</h1>
+//         <p>CD checkpoint - GH connected</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Wrapper>
+            <Route exact path="/" component={Landing} />
+          </Wrapper>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
+
 
 export default App;
