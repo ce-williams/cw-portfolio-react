101dbcd91dbd33d8c86f0805a3583d0cb5a4b546
cewilliams <cewilliams@csbsju.edu>
2019-05-13 21:55:51 -0500

    add and remove test components, changed background and cleanup misc elements

--- src/App.js
+++ src/App.js
@@ -2,44 +2,25 @@
 // import logo from './logo.svg';
 import './App.css';
 
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from "react";
+
 import Landing from './components/Landing';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <h1>CW portfolio</h1>
-//         <p>CD checkpoint - GH connected</p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-class App extends Component {
+
+class App extends React.Component {
   render() {
     return (
+      
       <Router>
         <div>
           <Wrapper>
             <Route exact path="/" component={Landing} />
           </Wrapper>
-          <Footer />
         </div>
       </Router>
     );
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,41 +1,82 @@
 import React from "react";
-import Row from "../Row/Row";
-// import Col from "../Column/Column";
-// import { Button } from 'react-bootstrap';
-// import TabBar from "../TabBar";
-// import { Link } from 'react-router-dom';
-// import { NavLink } from 'react-router-dom';
 import "./Landing.css";
 
 
 const Landing = () => (
 
   <div>
-    {/* <img className="Logo" alt="roamr logo" src={"https://github.com/zorporate/final-project/blob/master/client/src/ROAMR-logo.png?raw=true"} /> */}
     
     <div className="Buttons">
     <h2>
         welcome
     </h2>
     <br></br>
-      <Row>
-        
-        {/* <Col size="md-12">
-          <Button bsStyle="success">
-            <NavLink to="/Login">Login</NavLink>
-          </Button>
-        </Col> */}
-      </Row>
-      <br></br>
-      <Row>
-        {/* <Col size="md-12">
-          <Button bsStyle='success' to="/Registration">
-            <NavLink to="/Registration">Register</NavLink>
-          </Button>
-        </Col> */}
-      </Row>
+     
     </div>
   </div>
 );
 
 export default Landing;
+
+// class Landing extends React.Component {
+//   // state = { active: 0}
+
+//   // componentDidMount() {
+//   //   window.addEventListener("scroll", this.handleScroll);
+//   // }
+
+//   // componentWillUnmount() {
+//   //   window.removeEventListener("scroll", this.handleScroll);
+//   // }
+
+//   // handleScroll = event => {
+//   //   const { pageYOffset } = window;
+//   //   const { active } = this.state;
+
+//   //   if (pageYOffset >= 10 && active === 0) {
+//   //     this.setState({ active: 1});
+//   //   } else if (pageYOffset < 10 && active === 1) {
+//   //     this.setState({ active: 0 })
+//   //   }
+//   // };
+  
+//   render() {
+//     // const { active } = this.state;
+
+//     let page = this.props.location.pathname.substr(1);
+//     if (!page) page = 'home';
+
+//     return(
+//       <div>
+//        <ReactCSSTransitionGroup
+//           transitionName="background"
+//           transitionEnterTimeout={1000}
+//           transitionLeaveTimeout={1000}
+//         >
+//           <BackgroundImage page={page} key={page} />
+//         </ReactCSSTransitionGroup>
+//       </div>
+      
+//       // <div style={{ height: 100, width: 100 }}>
+//       //   <div
+//       //     style={{
+//       //       height: "100%",
+//       //       width: "100%",
+//       //       backgroundColor: "red",
+//       //       display: active === 0 ? "block" : "none"
+//       //     }}
+//       //   />
+//       //   <div
+//       //     style={{
+//       //       height: "100%",
+//       //       width: "100%",
+//       //       backgroundColor: "blue",
+//       //       display: active === 1 ? "block" : "none"
+//       //     }}
+//       //   />
+//       // </div>
+
+//     );
+//   }
+// }
+
