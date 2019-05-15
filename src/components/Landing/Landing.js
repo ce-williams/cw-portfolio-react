409ce1f1f4f4e7972af54527f2496dcba5486ab5
cewilliams <cewilliams@csbsju.edu>
2019-05-14 22:00:31 -0500

    cards added to container, bootstrap HTML link

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,38 +1,88 @@
 import React from "react";
 import "./Landing.css";
-// import Container from "./Container"
 import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
+
 
 
 const Landing = () => (
 
   <div>
-    
+
     <div className="Buttons">
-    <h2>
+      <h2>
         welcome
     </h2>
     <Container>
-      <Row>
-        <Col>1 of 2</Col>
-        <Col>2 of 2</Col>
-      </Row>
-      <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row>
-    </Container>
       
-    />
+      
+      <Row>
+        <Col>
+          <CardDeck>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This is a wider card with supporting text below as a natural lead-in to
+                  additional content. This content is a little bit longer.
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </CardDeck>
+        </Col>
 
-    
-    
-     
+        <Col>
+          <CardDeck>
+            <Card>
+              <Card.Img variant="top" src="holder.js/100px160" />
+              <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                  This card has supporting text below as a natural lead-in to additional
+                  content.{' '}
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          </CardDeck>
+        </Col>
+        
+        <Col>
+          <CardDeck>
+            <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural lead-in to
+                additional content. This card has even longer content than the first to
+                show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+            </Card>
+          </CardDeck>
+        </Col>
+      </Row>
+      
+    </Container>
+
+
     </div>
   </div>
 );
 
 export default Landing;
+
+
