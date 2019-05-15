b5c6b35f7fd0dbec3339e403173c46c29c066ee4
cewilliams <cewilliams@csbsju.edu>
2019-05-14 21:39:39 -0500

    new background, React container, rows & columns bootstrap

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Landing.css";
+// import Container from "./Container"
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
 
 
 const Landing = () => (
@@ -10,73 +14,25 @@ const Landing = () => (
     <h2>
         welcome
     </h2>
-    <br></br>
+    <Container>
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+      
+    />
+
+    
+    
      
     </div>
   </div>
 );
 
 export default Landing;
-
-// class Landing extends React.Component {
-//   // state = { active: 0}
-
-//   // componentDidMount() {
-//   //   window.addEventListener("scroll", this.handleScroll);
-//   // }
-
-//   // componentWillUnmount() {
-//   //   window.removeEventListener("scroll", this.handleScroll);
-//   // }
-
-//   // handleScroll = event => {
-//   //   const { pageYOffset } = window;
-//   //   const { active } = this.state;
-
-//   //   if (pageYOffset >= 10 && active === 0) {
-//   //     this.setState({ active: 1});
-//   //   } else if (pageYOffset < 10 && active === 1) {
-//   //     this.setState({ active: 0 })
-//   //   }
-//   // };
-  
-//   render() {
-//     // const { active } = this.state;
-
-//     let page = this.props.location.pathname.substr(1);
-//     if (!page) page = 'home';
-
-//     return(
-//       <div>
-//        <ReactCSSTransitionGroup
-//           transitionName="background"
-//           transitionEnterTimeout={1000}
-//           transitionLeaveTimeout={1000}
-//         >
-//           <BackgroundImage page={page} key={page} />
-//         </ReactCSSTransitionGroup>
-//       </div>
-      
-//       // <div style={{ height: 100, width: 100 }}>
-//       //   <div
-//       //     style={{
-//       //       height: "100%",
-//       //       width: "100%",
-//       //       backgroundColor: "red",
-//       //       display: active === 0 ? "block" : "none"
-//       //     }}
-//       //   />
-//       //   <div
-//       //     style={{
-//       //       height: "100%",
-//       //       width: "100%",
-//       //       backgroundColor: "blue",
-//       //       display: active === 1 ? "block" : "none"
-//       //     }}
-//       //   />
-//       // </div>
-
-//     );
-//   }
-// }
-
